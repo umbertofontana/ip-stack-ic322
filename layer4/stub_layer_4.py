27f31e0331123bdc33f4cefc2ac27a5de7461e68
Umberto Fontana <m238310@usna.edu>
2022-10-22 20:30:07 -0400

    Added comments and organized code

--- layer4/stub_layer_4.py
+++ layer4/stub_layer_4.py
@@ -1,25 +1,22 @@
-# This is a stub for Layer 4.
+# Layer 4 Protocol
 
 # Import Layers
+# Why are we not importing Layer 5? 
 import sys
 sys.path.append('../')
 import layer3.stub_layer_3 as Layer3
 import logging
 
 class StubLayer4:
+    # Create a Layer 4 Object. The __init__ function is called automatically whenever a new object of a class is created. 
+    # It's used to initialize the attributes of the class.
     def __init__(self):
-        """Create a Layer 4 Object.
-
-            This initializer doesn't ask for a callback since there
-            will likely be multiple callbacks connected to sockets.
-            Use the `connect_to_socket` method to register a
-            layer_5_cb.
-
-        """
-
         # Connect to Layer 3
         self.layer3 = Layer3.StubLayer3(self.from_layer_3)
+    
+    # -------------------------------------------------------------------------------------------------------------------------------------
 
+    # Connect an application to a socket.
     def connect_to_socket(self, port_number, layer_5_cb):
         """Connect an application to a socket.
 
@@ -34,32 +31,33 @@ class StubLayer4:
 
         # For now, just send all messages to the latest open socket.
         self.layer_5_cb = layer_5_cb
+        logging.debug(f"{self.layer_5_cb}")
 
+    # -------------------------------------------------------------------------------------------------------------------------------------
 
+    # Call this function to send data to this Layer from Layer 5.
+    # This is the function called from Layer 5 when it wants to send us data. It's basically the function that sends a message.
     def from_layer_5(self, data, src_port, dest_port, dest_addr):
-        """Call this function to send data to this layer from layer 5
-
-        `src_port` - Integer. This is the port number on this machine
-        `dest_port` - Integer. This is the port number on the
-                      receiver's machine.
-        """
+      
         logging.debug(f"Layer 4 received msg from Layer 5: {data}")
 
-        # Right now I'm not doing anything with the port numbers.
-        # You should use them to multiplex between applications.
+        # This is the place to implement an header to attach to the data: it must contain the destination address, destination port 
+        # and the source port. Port numbers are necessary to multiplex between applications.
+        # This is also the place to implement a timer.
 
         # Pass the message down to Layer 3
         self.layer3.from_layer_4(data) 
+    
+    # -------------------------------------------------------------------------------------------------------------------------------------
 
+    # Call this function to send data to this Layer from Layer 3
+    # This is the function called from Layer 3 when it wants to send us data. It's the function that receives a message.
     def from_layer_3(self, data):
-        """Call this function to send data to this layer from layer
-        3
-        """
+       
         logging.debug(f"Layer 4 received msg from Layer 3: {data}")
 
-        # You need to take Layer 3 data and forward it to Layer 5.
-        # However, there might be multiple Layer 5 applications you could
-        # send the data to. How are you going to multiplex these?
+        # This is where I need to understand the header, remove it and implement multiplexing using the port numbers.
+        # Here is also where I need to send an ACK back.
 
-        # Let's just forward this up to layer 5.
+        # Pass the message up to Layer 5
         self.layer_5_cb(data)
