c90dfa169f339746855a4d857ffd40171ed2ca86
Umberto Fontana <umbertofontana362@gmail.com>
2022-12-08 21:51:50 -0500

    new run-app

--- layer5/run-applications.py
+++ layer5/run-applications.py
@@ -8,6 +8,7 @@ sys.path.append('../')
 import layer4.layer4 as Layer4
 import l4_simple_app as SimpleApp # Remember to change this when we merge
 import logging
+from tkinter import *
 
 # Chat: port 0
 # Weapon: port 1
@@ -19,27 +20,35 @@ logging.basicConfig(level=logging.DEBUG)
 
 layer4 = Layer4.StubLayer4()
 
-def start_chat():
+def start_chat(mywin, sender):
     chatport = 0 # The port where the chat client communicates is hardcoded as 0
-    chat = SimpleApp.SimpleApp(chatport, layer4)
+    chat = SimpleApp.SimpleApp(chatport, layer4, mywin, sender)
     print("Server online.")
     while True:
+        pass
+        '''
         message = input("Msg: ")
         destport = int(input("Chat or weapon? <0: chat, 1: weapon control>"))
         destaddress = int(input("To which address? <0 through 4>"))
         # We haven't implemented an address scheme, so we will just pass the receiver_addr = None.
         chat.send(message, destport, chatport, destaddress)
+        '''
         
-def start_weapon():
+def start_weapon(mywin, sender):
     # The weapon only listens to feedback at the moment
     weaponport = 1 # The port where the weapon control communicates is hardcoded as 1
-    weapon = SimpleApp.SimpleApp(weaponport, layer4)
+    weapon = SimpleApp.SimpleApp(weaponport, layer4, mywin, sender)
     while True:
         pass
 
-# Start the chat thread
-t = Thread(target=start_chat, args = ())
+window=Tk()
+window.title('WEAPON LAUNCH')
+window.geometry("400x300+10+10")
+
+t = Thread(target=start_chat, args = (window,True))
 t.start()
-# Start the weapon thread
-t = Thread(target=start_weapon, args = ())
+t = Thread(target=start_weapon, args = (window,False))
 t.start()
+
+window.mainloop()
+
